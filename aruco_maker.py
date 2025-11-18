5d174936ff0004592c229c9dd32a36e396ea06e1
jieun02-kim <jieun0910@mju.ac.kr>
2025-11-19 01:09:22 +0900

    please

--- aruco_maker.py
+++ aruco_maker.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     
     board_type=cv2.aruco.DICT_4X4_50;
     MARKER_SIZE = 400;
-    id_info = 1;
+    id_info = 3;
     
     arucoDict = cv2.aruco.getPredefinedDictionary(board_type);
     aruco_matker_img = cv2.aruco.generateImageMarker(arucoDict , id_info , MARKER_SIZE);
